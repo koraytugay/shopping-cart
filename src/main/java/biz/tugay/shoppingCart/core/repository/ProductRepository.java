87358a5f102da2f4af7663fd1e30cfc5b228148c
koraytugay <ktugay@sonatype.com>
2020-05-17 18:42:24 -0400

    Formatting only

--- src/main/java/biz/tugay/shoppingCart/core/repository/ProductRepository.java
+++ src/main/java/biz/tugay/shoppingCart/core/repository/ProductRepository.java
@@ -3,7 +3,8 @@ package biz.tugay.shoppingCart.core.repository;
 import biz.tugay.shoppingCart.core.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, String>
+public interface ProductRepository
+    extends JpaRepository<Product, String>
 {
   Product findDistinctBySku(String sku);
 }
